a7b16e1c688bc4703ea6626803df2f680bd1bd5f
Matheus Hora <matheus11hora@gmail.com>
2022-03-28 12:01:39 -0300

    Novos

---
+++ JS basico/ex01.js
@@ -0,0 +1,1 @@
+console.log('Meu nome é "Matheus". estou aprendendo JavaScript as ' + 11 +' da manhã')
---
+++ JS basico/ex02.js
@@ -0,0 +1,12 @@
+let nome = 'Matheus'
+let sobreNome = 'Hora'
+let idade = 20
+let peso = 75
+let altura = 1.70
+let imc = peso/ (altura * altura)
+let nascimento = 2022 - idade
+
+//dois jeitos de imprimir as string + variaveis
+console.log(nome + ' ' + sobreNome + ' tem ' + idade + ' , pesa ' + peso + ' tem ' + altura + ' de altura e seu IMC é de ' + imc)
+
+console.log(`${nome} ${sobreNome} nasceu em ${nascimento}`)
