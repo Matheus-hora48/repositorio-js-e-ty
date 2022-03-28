654cff61df0cc18dc04c1e3401a59805e0b99e5b
Matheus Hora <matheus11hora@gmail.com>
2022-03-28 16:52:35 -0300

    Novos

---
+++ JS basico/ex03/ex03.js
@@ -0,0 +1,10 @@
+//parseint converte string em numero inteiro
+
+let num1 = prompt('Digite o 1 numero')
+let num2 = prompt('Digite o 2 numero')
+
+num1 = Number(num1)
+num2 = Number(num2)
+
+const resul = num1 + num2
+alert(`O resultado da soma é ${resul}`)
