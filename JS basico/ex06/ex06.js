9c7b0b975b9ba07ecf78c6d95c0f1fe8f21ec878
Matheus Hora <matheus11hora@gmail.com>
2022-03-28 23:51:10 -0300

    Novos

---
+++ JS basico/ex06/ex06.js
@@ -0,0 +1,7 @@
+let num = prompt('Digite um numero')
+
+let raiz = Math.pow(num, 2)
+let maior = Math.ceil(num)
+let menor = Math.floor(num)
+
+console.log(raiz)
---
+++ JS basico/ex07/ex07.js
@@ -0,0 +1,3 @@
+let num = prompt('Digite um numero')
+
+let raiz = Math.SQRT2(num)
