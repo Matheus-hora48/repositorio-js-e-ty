9d32d2dc8194a58399d0bc83d6b0816d85f107b2
Matheus Hora <matheus11hora@gmail.com>
2022-03-29 18:00:03 -0300

    Novos

---
+++ Logica/operadores/comparacao.js
@@ -0,0 +1,12 @@
+/*
+Operadores de comparação
+> maior que
+>= maior que ou igual a 
+< menor que
+<= menor que ou igual a
+== igualdade (valor)
+=== igualdade restrita (valor e tipo)
+!= diferente (valor)
+!== diferente restrito (valor e tipo)
+*/
+console.log(10>5)
