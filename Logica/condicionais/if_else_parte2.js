3b269895fc3612f9282855982784b986b6334cb6
Matheus Hora <matheus11hora@gmail.com>
2022-03-30 11:23:54 -0300

    Novos

---
+++ Logica/condicionais/if_else_parte2.js
@@ -0,0 +1,12 @@
+const hora = Math.random() * 24;
+
+console.log(hora)
+if(hora >= 0 && hora <= 11){
+  console.log('Bom dia')
+} else if (hora >= 12 && hora <= 17) {
+  console.log('Boa tarde')
+} else if (hora >= 18 && hora <= 23) {
+  console.log('Boa noite')
+} else {
+  console.log('hora invalida')
+}
