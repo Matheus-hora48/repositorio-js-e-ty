77f87b19d15b3f4a6040b90d7acbfd564028ed57
Matheus Hora <matheus11hora@gmail.com>
2022-03-31 12:00:10 -0300

    Novos

---
+++ Logica/operadores/ternario.js
@@ -0,0 +1,5 @@
+// ? :
+const pontuacao = 999
+//depois do ? é a parte verdadeira e depois do : a falsa
+const nivelUser = pontuacao >= 1000 ? 'Usuario VIP' : 'Usuario normal'
+console.log(nivelUser)
