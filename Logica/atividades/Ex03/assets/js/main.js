df70849e08957ad18027a9673d997bb61ce8be7e
Matheus Hora <matheus11hora@gmail.com>
2022-04-02 11:30:36 -0300

    Novos

--- Logica/atividades/Ex03/assets/js/main.js
+++ Logica/atividades/Ex03/assets/js/main.js
@@ -1,8 +1,18 @@
 const elementos = [
-  {tag: 'p', texto: 'Frase 1'},
-  {tag: 'div', texto: 'Frase 2'}, 
-  {tag: 'footer', texto: 'Frase 3'},
-  {tag: 'section', texto: 'Frase 4'},
+  {tag: 'p', texto: 'Texto incial'},
+  {tag: 'div', texto: 'Texto 1'}, 
+  {tag: 'footer', texto: 'Texto 2'},
+  {tag: 'section', texto: 'Texto final'},
 ];
 
-const container = document.querySelector('container')
+const container = document.querySelector('.container')
+const div = document.createElement('div')
+
+for(let i = 0; i < elementos.length; i++){
+  let {tag, texto} = elementos[i]
+  let tags = document.createElement(tag)
+  tags.innerHTML = texto
+  div.appendChild(tags)
+}
+
+container.appendChild(div)
