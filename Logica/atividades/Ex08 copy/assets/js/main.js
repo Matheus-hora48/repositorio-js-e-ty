723ceaa1336e460581bdaaa6d66aa85bce33d7ad
Matheus Hora <matheus11hora@gmail.com>
2022-04-04 16:06:55 -0300

    Novos

---
+++ Logica/atividades/Ex08 copy/assets/js/main.js
@@ -0,0 +1,38 @@
+function criaSegundos(segundos) {
+  let data = new Date(segundos * 1000)
+  return data.toLocaleTimeString('pt-BR', {
+    hour12: false,
+    timeZone: 'GMT'
+  })
+}
+
+const relogio = document.querySelector('.relogio')
+const iniciar = document.querySelector('.iniciar')
+const pausar = document.querySelector('.pausar')
+const zerar = document.querySelector('.zerar')
+let segundos = 0
+let timer
+
+function iniciaRelogio() {
+  timer = setInterval(function () {
+    segundos++
+    relogio.innerHTML = criaSegundos(segundos)
+  }, 1000)
+}
+
+iniciar.addEventListener('click', function (e) {
+  relogio.classList.remove('pausado')
+  clearInterval(timer)
+  iniciaRelogio()
+})
+
+pausar.addEventListener('click', function (e) {
+  clearInterval(timer)
+  relogio.classList.add('pausado')
+})
+
+zerar.addEventListener('click', function (e) {
+  clearInterval(timer)
+  relogio.innerHTML = '00:00:00'
+  segundos = 0  
+})
