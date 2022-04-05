3792966921b621c30bf37a1b8cd5671b5633f641
Matheus Hora <matheus11hora@gmail.com>
2022-04-05 12:00:33 -0300

    Novos

---
+++ Função/maneiras_de_declarar.js
@@ -0,0 +1,13 @@
+//Declaração de função
+function falaOi() {
+  console.log('oi')
+}
+falaOi()
+
+//voce pode criar uma função com um dado
+
+const dado = function (){
+  console.log('sou um dado')
+}
+
+dado()
