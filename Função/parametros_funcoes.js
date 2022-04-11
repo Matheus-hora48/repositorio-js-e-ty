4ed597d6d31d03b5cebd1b65a25eedf8308c08f9
Matheus Hora <matheus11hora@gmail.com>
2022-04-11 17:52:30 -0300

    Novos

---
+++ Função/parametros_funcoes.js
@@ -0,0 +1,28 @@
+//assume um valor padrao para as variavei
+function funcao(a , b = 3 , c = 4){
+
+  console.log(a + b + c)
+}
+
+//assim ele pega o valor padrao do sistema
+funcao(2, undefined , 10)
+
+/*
+function funcao(a,b,c,d,e,f){
+console.log(a,b,c,d,e,f)
+}
+
+funcao(1,2,3)
+
+
+function funcao(){
+  let total = 0
+  for (let argumentos of arguments){
+    total += argumentos
+  }
+
+  console.log(total)
+}
+
+funcao(1,2,3,4,5,6,7,8,9)
+*/
