0075fe5eecd791f266c49f374f1120d0f1d6c745
Matheus Hora <matheus11hora@gmail.com>
2022-04-11 17:59:33 -0300

    Novos

--- Função/parametros_funcoes.js
+++ Função/parametros_funcoes.js
@@ -1,3 +1,9 @@
+function funcao({nome, sobrenome, idade}){
+  console.log(nome, sobrenome, idade)
+}
+
+
+/*
 //assume um valor padrao para as variavei
 function funcao(a , b = 3 , c = 4){
 
@@ -7,7 +13,7 @@ function funcao(a , b = 3 , c = 4){
 //assim ele pega o valor padrao do sistema
 funcao(2, undefined , 10)
 
-/*
+
 function funcao(a,b,c,d,e,f){
 console.log(a,b,c,d,e,f)
 }
