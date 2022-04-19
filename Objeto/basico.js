fdde3d2051228eaaac78dd00fd6fcaf9b0a2c367
Matheus Hora <matheus11hora@gmail.com>
2022-04-19 18:00:10 -0300

    Novos

---
+++ Objeto/basico.js
@@ -0,0 +1,15 @@
+const pessoa = {
+  nome: 'Matheus',
+  sobrenome: 'Hora',
+}
+
+const chave = 'nome'
+
+
+
+
+
+
+//so com a notação de chave
+console.log(pessoa[chave])
+console.log(pessoa.sobrenome)
