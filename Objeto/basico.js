fcba47339b54e76913cb000358d34717e0e691bb
Matheus Hora <matheus11hora@gmail.com>
2022-04-20 10:53:45 -0300

    Novos

--- Objeto/basico.js
+++ Objeto/basico.js
@@ -6,9 +6,21 @@ const pessoa = {
 const chave = 'nome'
 
 
+function Pessoa(nome, sobrenome){
+  this.nome = nome
+  this.sobrenome = sobrenome
+
+  
+}
+
+//o new cria um novo objeto
+const p1 = new Pessoa('Matheus', 'Hora')
+//serve para travar o objeto e empedir de mudar os codigos
+Object.freeze(p1)
+p1.nome = 'Outra'
 
 
-
+console.log(p1)
 
 //so com a notação de chave
 console.log(pessoa[chave])
