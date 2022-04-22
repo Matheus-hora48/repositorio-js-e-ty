201515762030f7106f9f0cf2f34191e7e6226813
Matheus Hora <matheus11hora@gmail.com>
2022-04-22 11:10:02 -0300

    Novos

--- Objeto/prototype.js
+++ Objeto/prototype.js
@@ -1,8 +1,16 @@
+//serve para colocar um parametro em um objeto que sempre que criar uma tributo ele vai vim junto
+
 function Pessoa(nome, sobrenome){
   this.nome = nome
   this.sobrenome = sobrenome
-  this.nomeCompleto = () => this.nome + ' ' + this.sobrenome
+  //this.nomeCompleto = () => 'Originas' + this.nome + ' ' + this.sobrenome
+}
+
+Pessoa.prototype.nomeCompleto = function(){
+  return this.nome + ' ' + this.sobrenome
 }
 
 const pessoa1 = new Pessoa('Matheus', 'O.')
 const data = new Date()
+
+console.dir(pessoa1)
