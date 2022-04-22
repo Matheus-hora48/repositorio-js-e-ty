c2e330bdaa02c3f58a07e35305ddb29695d6df4f
Matheus Hora <matheus11hora@gmail.com>
2022-04-22 17:34:10 -0300

    Novos

---
+++ Objeto/heranca.js
@@ -0,0 +1,22 @@
+function Produto(nome, preco){
+  this.nome = nome
+  this.preco = preco
+}
+
+Produto.prototype.aumento = function (quantia){
+  this.preco += quantia
+}
+
+Produto.prototype.desconto = function (quantia){
+  this.preco -= quantia
+}
+
+function Camiseta(nome, preco, cor){
+  Produto.call(this, nome, preco)
+}
+
+Camiseta.prototype = Object.create(Produto.prototype)
+Camiseta.prototype.constructor = Camiseta
+
+const camiseta = new Camiseta('Regata', 20 , 'Vermelha')
+console.log(camiseta)
