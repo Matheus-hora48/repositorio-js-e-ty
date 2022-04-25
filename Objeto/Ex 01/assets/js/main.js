77675b7a9b9645c473734a8614e9d1defbee87b6
Matheus Hora <matheus11hora@gmail.com>
2022-04-25 12:01:19 -0300

    Mudanças

--- Objeto/Ex 01/assets/js/main.js
+++ Objeto/Ex 01/assets/js/main.js
@@ -1,41 +1,68 @@
-let cpf = '00747303800'
+let cpf = '070.903.015.08'
 let cpfLimpo = cpf.replace(/\D+/g, '')
 cpfArray = Array.from(cpfLimpo)
 
-let cpfParcial = cpfArray.slice(0, -2)
-let regressivo1 = cpfParcial.length + 1
-const total = cpfParcial.reduce((ac, val) => {
-  ac += (regressivo1* Number(val))
-  regressivo1--
-  return ac
-}, 0)
+//fazendo a validação
+function sequencia(cpf) {
+  const sequencia = cpf[0].repeat(cpf.length)
+  return sequencia === cpf
+}
+
+function validando(cpf) {
+  if (typeof cpf === 'undefined') return false
+  if (cpf.length !== 11) return false
+  if (sequencia(cpf)) return false
+}
+
+const cpfParcial = cpfArray.slice(0, -2)
+const regressivo = cpfParcial.length + 1
+
+//funções que vai fz o calculo
+function primeiroNumero(parcial, regressivo) {
+  const total = parcial.reduce((ac, val) => {
+    ac += regressivo * Number(val)
+    regressivo--
+    return ac
+  }, 0)
+
+  let digito = 11 - (total % 11)
+  return digito > 9 ? '0' : digito
+}
+
+const pri = primeiroNumero(cpfParcial, regressivo)
+const priArr = adicionadoPrimeiroDigito(cpfParcial, pri)
+
+const segundoRegressivo = cpfParcial.length + 1
+const seg = segundoNumero(cpfParcial, segundoRegressivo)
+const segArr = adicionadoSegundoDigito(cpfParcial, seg)
+
+console.log(Number(cpfLimpo))
+//const teste = cpfParcial == cpfArray ? true :false
+
+//console.log(teste)
 
 
-let digito = 11 - (total % 11) 
-if(digito > 9) {
-  digito = 0
-} else digito
+
+function adicionadoPrimeiroDigito(parc, total) {
+  return parc.push(total.toString())
+}
+
+function segundoNumero(parcial, regressivo) {
+  const total = parcial.reduce((ac, val) => {
+    ac += regressivo * Number(val)
+    regressivo--
+    return ac
+  }, 0)
+
+  let digito = 11 - (total % 11)
+  return digito > 9 ? '0' : digito
+}
 
 
-let cpfParcial2 = cpfParcial
-cpfParcial2.push(digito.toString())
-regressivo2 = cpfParcial.length + 1
+function adicionadoSegundoDigito(parc, total) {
+  return parc.push(total.toString())
+}
 
+function novoCpf(cpfVelho){
 
-
-const total2 = cpfParcial2.reduce((ac, val) => {
-  ac += (regressivo2* Number(val))
-  regressivo2--
-  return ac
-},0)
-
-let digito2 = 11 - (total2 % 11) 
-if(digito2 > 9) {
-  digito2 = 0
-} else digito2
-
-cpfParcial2.push(digito2.toString())
-console.log(cpfArray)
-console.log(cpfParcial2)
-
-const teste = cpfArray = cpfParcial2 ? true: false 
+}
