ec3b4470214b4b1b30d7036a0e7e5e9fb0d1fca7
Matheus Hora <matheus11hora@gmail.com>
2022-04-25 17:39:33 -0300

    novos

--- Objeto/factory_functions.js
+++ Objeto/factory_functions.js
@@ -1,23 +1,27 @@
-function criaPessoa(nome, sobrenome){
-
-  const pessoafrototype = {
-    falar(){
-      console.log(`${this.nome} está falando`)
-    },
-
-    comer(){
-      console.log(`${this.nome} está cpmendo`)
-    },
-
-    beber(){
-      console.log(`${this.nome} está bebendo`)
-    },
+const falar = {
+  falar() {
+    console.log(`${this.nome} está falando`)
   }
-
-  return{
-    nome,
-    sobrenome
+}
+const comer = {
+  comer() {
+    console.log(`${this.nome} está cpmendo`)
+  },
+}
+const beber = {
+  beber() {
+    console.log(`${this.nome} está bebendo`)
   }
 }
 
+const pessoafrototype = {...falar, ...comer, ...beber}
+
+function criaPessoa(nome, sobrenome) {
+  return Object.create(pessoafrototype, {
+    nome: { value: nome },
+    sobrenome: { value: sobrenome }
+  })
+}
+
 const p1 = criaPessoa('Matheus', 'Hora')
+console.log(p1)
