a28a4fef0fd08aad4ba5be3cbc3e6b66dd3611d7
Matheus Hora <matheus11hora@gmail.com>
2022-04-26 15:12:54 -0300

    Novos

---
+++ Objeto/classe/Ex 02/assets/js/main.js
@@ -0,0 +1,44 @@
+class ValidaFormulario {
+  constructor() {
+    this.formulario = document.querySelector('.formulario')
+
+    this.eventos()
+  }
+
+  eventos() {
+    this.formulario.addEventListener('submit', e => {
+      this.handleSubmit(e)
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    const camposValidos = this.checkFilds()
+  }
+
+  checkFilds() {
+    let valid = true
+
+    for (let errorText of this.formulario.querySelectorAll('.error-text')) {
+      errorText.remove()
+    }
+
+    for (let campo of this.formulario.querySelectorAll('.validar')) {
+      const label = campo.previousElementSibling.innerHTML
+
+      if (!campo.value) {
+        this.criaErro(campo, `Campo "${label}" não pode estar em branco. `)
+        valid = false
+      }
+    }
+  }
+
+  criaErro(campo, msg) {
+    const div = document.createElement('div')
+    div.innerHTML = msg
+    div.classList.add('error-text')
+    campo.insertAdjacentElement('afterend', div)
+  }
+}
+
+const valida = new ValidaFormulario()
