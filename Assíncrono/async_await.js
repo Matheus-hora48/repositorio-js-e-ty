06bbcd263e368d200d6f8b53028d1075bb2834d5
Matheus Hora <matheus11hora@gmail.com>
2022-04-27 11:50:30 -0300

    Novos

--- Assíncrono/async_await.js
+++ Assíncrono/async_await.js
@@ -20,6 +20,7 @@ function esperaAi(msg, tempo) {
   })
 }
 
+/*
 esperaAi('Fase 1', rand(0,3))
 .then(valor => {
   console.log(valor)
@@ -33,3 +34,31 @@ esperaAi('Fase 1', rand(0,3))
   console.log(fase)
 })
 .catch(e => console.log(e))
+*/
+
+//serve para retornar esperando todas as promises
+
+//estado das promises
+//pending-> pendente, logo quando ela começa
+//fullfielled-> resolvida
+//rejected-> rejeitada
+
+async function executa(){
+  try{
+    const fase1 = await esperaAi('Fase 1', rand(0,3))
+    console.log(fase1)
+  
+    const fase2 = await esperaAi('Fase 2', rand(0,3))
+    console.log(fase2)
+  
+    const fase3 = await esperaAi('Fase 3', rand(0,3))
+    console.log(fase3)
+  
+    console.log('Terminando na fase:', fase3)
+  } catch(e){
+    console.log(e)
+  }
+  
+}
+
+executa()
