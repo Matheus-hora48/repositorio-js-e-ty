891cc56d38c46db92926f7fffe743089c9ca001b
Matheus Hora <matheus11hora@gmail.com>
2022-04-28 17:59:11 -0300

    Novos

--- Alguns projetos/gerador_senha/src/modules/formGeraSenha.js
+++ Alguns projetos/gerador_senha/src/modules/formGeraSenha.js
@@ -1,27 +1,27 @@
 import geraSenha from './geradores';
 
-const senhaGerada = document.querySelector('.senha-gerada');
-const qtdCaracteres = document.querySelector('.qtd-caracteres');
-const chkMaiusculas = document.querySelector('.chk-maiusculas');
-const chkMinusculas = document.querySelector('.chk-minusculas');
-const chkNumeros = document.querySelector('.chk-numeros');
-const chkSimbolos = document.querySelector('.chk-simbolos');
-const gerarSenha = document.querySelector('.gerar-senha');
+const senhaGerada = document.querySelector('.senha-gerada')
+const qtdCaracteres = document.querySelector('.qtd-caracteres')
+const chkMaiusculas = document.querySelector('.chk-maiusculas')
+const chkMinusculas = document.querySelector('.chk-minusculas')
+const chkNumeros = document.querySelector('.chk-numeros')
+const chkSimbolos = document.querySelector('.chk-simbolos')
+const gerarSenha = document.querySelector('.gerar-senha')
 
 export default () => {
-  gerarSenha.addEventListener('click', () => {
-    senhaGerada.innerHTML = gera();
-  });
-};
+  gerarSenha.addEventListener('click', () =>{
+    senhaGerada.innerHTML = gera()
+  })
+}
 
-function gera() {
-  const senha = geraSenha(
+function gera(){
+  const senha = gerarSenha(
     qtdCaracteres.value,
     chkMaiusculas.checked,
     chkMinusculas.checked,
     chkNumeros.checked,
-    chkSimbolos.checked
-  );
+    chkSimbolos.checked,
+  )
 
-  return senha || 'Nada selecionado.';
+  return senha
 }
--- Alguns projetos/gerador_senha/src/modules/geradores.js
+++ Alguns projetos/gerador_senha/src/modules/geradores.js
@@ -1,20 +1,22 @@
 const rand = (min, max) => Math.floor(Math.random() * (max - min) + min);
-const geraMaiuscula = () => String.fromCharCode(rand(65, 91));
-const geraMinuscula = () => String.fromCharCode(rand(97, 123));
-const geraNumero = () => String.fromCharCode(rand(48, 58));
-const simbolos = ',.;~^[]{}!@#$%*()_+=-';
-const geraSimbolo = () => simbolos[rand(0, simbolos.length)];
+const geraMaiuscula = () => String.fromCharCode(rand(65, 91))
+const geraMinuscula = () => String.fromCharCode(rand(97, 123))
+const geraNumero = () => String.fromCharCode(rand(48, 58))
+const simbolos = ',.;~^[]!@#$%*()_+=-'
+const geraSimbolo = () => simbolos[rand(0, simbolos.length)]
 
-export default function geraSenha(qtd, maiusculas, minusculas, numeros, simbolos) {
-  const senhaArray = [];
-  qtd = Number(qtd);
+export default function gerarSenha(qtd, Maiuscula, minusculas, numeros, simbolos){
+  const senhaArr = []
+  qtd = Number(qtd)
 
-  for(let i = 0; i < qtd; i++) {
-    maiusculas && senhaArray.push(geraMaiuscula());
-    minusculas && senhaArray.push(geraMinuscula());
-    numeros && senhaArray.push(geraNumero());
-    simbolos && senhaArray.push(geraSimbolo());
+  for(let i = 0; i < qtd; i++){
+    Maiuscula && senhaArr.push(geraMaiuscula())
+    minusculas && senhaArr.push(geraMinuscula())
+    numeros && senhaArr.push(geraNumero())
+    simbolos && senhaArr.push(geraSimbolo())
   }
 
-  return senhaArray.join('').slice(0, qtd);
+  return senhaArr.join('').slice(0, qtd)
 }
+
+geraSenha(10, true)
